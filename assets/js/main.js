08fa98be70333a52a19db7c1664bb9234e5539de
rosenness <hsnclk1985@hotmail.com>
2026-06-21 17:49:06 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,110 @@
+(function () {
+  //===== Prealoder
+
+  window.onload = function () {
+    window.setTimeout(fadeout, 500);
+  };
+
+  function fadeout() {
+    document.querySelector(".preloader").style.opacity = "0";
+    document.querySelector(".preloader").style.display = "none";
+  }
+
+  /*=====================================
+    Sticky
+    ======================================= */
+  window.onscroll = function () {
+    const header_navbar = document.querySelector(".navbar-area");
+    const sticky = header_navbar.offsetTop;
+    const logo = document.querySelector(".navbar-brand img");
+
+    const scrollText = document.querySelector(".scroll-only-text"); // Yazı elementini seçtik
+
+    if (window.pageYOffset > sticky) {
+        header_navbar.classList.add("sticky");
+        logo.src = "assets/img/logo/nart_renkli.svg"; // BURAYA DİKKAT: Sadece ikon olan versiyon
+        scrollText.style.display = "inline-block"; // Yazıyı göster
+    } else {
+        header_navbar.classList.remove("sticky");
+        logo.src = "assets/img/logo/nart_beyaz.svg"; // Beyaz logon (yazısız olan)
+        scrollText.style.display = "none"; // Yazıyı gizle
+    }
+//    if (window.pageYOffset > sticky) {
+//      header_navbar.classList.add("sticky");
+//      // logo.src = "assets/img/logo/logo-2.svg";
+//      logo.src = "assets/img/logo/nart_renkli.svg";
+//    } else {
+//      header_navbar.classList.remove("sticky");
+//      // logo.src = "assets/img/logo/logo.svg";
+//      logo.src = "assets/img/logo/nart_beyaz.svg";
+//    }
+
+    // show or hide the back-top-top button
+    const backToTo = document.querySelector(".scroll-top");
+    if (
+      document.body.scrollTop > 50 ||
+      document.documentElement.scrollTop > 50
+    ) {
+      backToTo.style.display = "flex";
+    } else {
+      backToTo.style.display = "none";
+    }
+  };
+
+  // for menu scroll
+  const pageLink = document.querySelectorAll(".page-scroll");
+
+  pageLink.forEach((elem) => {
+    elem.addEventListener("click", (e) => {
+      e.preventDefault();
+      document.querySelector(elem.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+        offsetTop: 1 - 60,
+      });
+    });
+  });
+
+  // section menu active
+  function onScroll(event) {
+    const sections = document.querySelectorAll(".page-scroll");
+    const scrollPos =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop;
+
+    for (let i = 0; i < sections.length; i++) {
+      const currLink = sections[i];
+      const val = currLink.getAttribute("href");
+      const refElement = document.querySelector(val);
+      const scrollTopMinus = scrollPos + 73;
+      if (
+        refElement.offsetTop <= scrollTopMinus &&
+        refElement.offsetTop + refElement.offsetHeight > scrollTopMinus
+      ) {
+        document.querySelector(".page-scroll").classList.remove("active");
+        currLink.classList.add("active");
+      } else {
+        currLink.classList.remove("active");
+      }
+    }
+  }
+
+  window.document.addEventListener("scroll", onScroll);
+
+  //===== close navbar-collapse when a  clicked
+  let navbarToggler = document.querySelector(".navbar-toggler");
+  const navbarCollapse = document.querySelector(".navbar-collapse");
+
+  document.querySelectorAll(".page-scroll").forEach((e) =>
+    e.addEventListener("click", () => {
+      navbarToggler.classList.remove("active");
+      navbarCollapse.classList.remove("show");
+    })
+  );
+  navbarToggler.addEventListener("click", function () {
+    navbarToggler.classList.toggle("active");
+  });
+
+  // WOW active
+  new WOW().init();
+})();
